a61c03729e64ce5eb2732eca40ae64f0b3482047
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2023-05-30 09:01:51 +0530

    Create 30_may 2693. Call Function with Custom Context.js

---
+++ 30_may 2693. Call Function with Custom Context.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Object} context
+ * @param {any[]} args
+ * @return {any}
+ */
+Function.prototype.callPolyfill = function(context, ...args) {
+  const fn = this;
+
+  Object.defineProperty(context, '__fn__', {
+    value: fn,
+    enumerable: false
+  });
+
+  const result = context.__fn__(...args);
+
+  delete context.__fn__;
+
+  return typeof result !== 'undefined' ? result : undefined;
+};

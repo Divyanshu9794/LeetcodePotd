6350b6408da4f5bc1c9845b53fd914e4d343c544
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2023-06-02 16:47:42 +0530

    Create 2648. Generate Fibonacci Sequence.js

---
+++ 2648. Generate Fibonacci Sequence.js
@@ -0,0 +1,24 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*() {
+    let a=0;
+    let b=1;
+  
+    yield a;
+    yield b;
+
+    
+    while(true){
+        let c= a+b;
+        yield c;
+        a=b;
+        b=c;
+    }
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */

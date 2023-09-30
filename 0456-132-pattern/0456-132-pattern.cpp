6da567a60ca065eae28e8cd277a7f6118197c0db
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2023-09-30 07:46:58 +0530

    Time: 62 ms (83.83%), Space: 50.7 MB (11.68%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        int n=nums.size();
+        if (n<3) return 0;
+        vector<int> n_k;
+        vector<int> n_i(n);
+        n_i[0]=nums[0];
+        for(int i=1; i<n; i++)
+            n_i[i]=min(n_i[i-1], nums[i]);
+        for(int j=n-1; j>0; j--){
+            if (nums[j]<=n_i[j]) continue;
+            while(!n_k.empty()&& n_k.back()<=n_i[j]) 
+                n_k.pop_back();
+            if(!n_k.empty() && n_k.back()<nums[j]) 
+                return 1;
+            n_k.push_back(nums[j]);
+        }
+        return 0;
+    }
+};

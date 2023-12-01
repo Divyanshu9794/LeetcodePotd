5cb465d39be81942b5863d4e88d131ce1df9cb42
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2023-12-01 09:26:49 +0530

    Time: 3 ms (60.50%) | Memory: 7.5 MB (31.09%) - LeetSync

---
+++ 2406-decode-the-message/decode-the-message.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string decodeMessage(string key, string message) {
+        map<char,char> m;
+        char start = 'a';
+        for(auto ch:key){
+            if(ch!=' '&& m[ch]==0){
+                m[ch]=start;
+                start++;
+            }
+        }
+        string ans;
+        for(auto ch:message){
+            if(ch==' '){
+                ans.push_back(ch);
+            }
+            else{
+                ans.push_back(m[ch]);
+            }
+        }
+        return ans;
+    }
+};

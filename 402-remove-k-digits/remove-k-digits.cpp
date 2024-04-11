8700bb1681f6dd0b64247b7ccd9a13c3910f982b
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2024-04-11 08:37:14 +0530

    Time: 13 ms (37.09%) | Memory: 9.7 MB (86.82%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        string res="";
+        
+        for(auto c : num){
+				while(res.length() && res.back()>c && k){ 
+                res.pop_back();
+                k--;
+            }
+            
+            if(res.length() || c!='0')
+                res.push_back(c);
+        }
+        
+        while(res.length() && k){
+            k--;
+            res.pop_back();
+        }
+        
+        return res.empty()? "0" : res;      
+    }
+};

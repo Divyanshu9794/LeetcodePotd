b152a81bfb1848447ba5888dc07735f95f8c9c8b
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2024-05-18 17:45:15 +0530

    Time: 4 ms (63.21%) | Memory: 15.4 MB (89.89%) - LeetSync

---
+++ 1021-distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+private:
+    int traverse (TreeNode* root, int& ans) {
+        if(root==NULL) return 0;
+        int coins = root->val + traverse (root->left, ans) + traverse (root->right, ans);
+        coins--;
+        ans += abs(coins);
+        return coins;
+    }
+public:
+    int distributeCoins(TreeNode* root) {
+        int ans = 0;
+        traverse (root, ans);
+        return ans;
+    }
+};

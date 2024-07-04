bc081598ebf4f4e83424f2511a84ee17693119ae
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2024-07-04 21:05:34 +0530

    Time: 458 ms (39.76%) | Memory: 278.1 MB (36.49%) - LeetSync

--- 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.cpp
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.cpp
@@ -8,23 +8,26 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
-public:
-    ListNode* mergeNodes(ListNode* head) {
-        ListNode* modify = head->next;
-        ListNode* nextSum = modify;
-
-        while (nextSum != nullptr) {
-            int sum = 0;
-            while (nextSum->val != 0) {
-                sum += nextSum->val;
-                nextSum = nextSum->next;
-            }
-            modify->val = sum;
-            nextSum = nextSum->next;
-            modify->next = nextSum;
-            modify = modify->next;
-        }
-        return head->next;
-    }
-};
+class Solution {  
+public:  
+    ListNode* mergeNodes(ListNode* head) {  
+        ListNode* dummy = new ListNode(0);  
+        ListNode* curr = dummy;  
+        int sum = 0;  
+  
+        while (head) {  
+            if (head->val == 0) {  
+                if (sum!= 0) {  
+                    curr->next = new ListNode(sum);  
+                    curr = curr->next;  
+                    sum = 0;  
+                }  
+            } else {  
+                sum += head->val;  
+            }  
+            head = head->next;  
+        }  
+  
+        return dummy->next;  
+    }  
+};  

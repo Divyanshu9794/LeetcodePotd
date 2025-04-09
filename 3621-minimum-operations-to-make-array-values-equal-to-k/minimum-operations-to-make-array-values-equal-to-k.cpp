fa4a69d8ce8f5a0dbe7b8ff57b866be4198db771
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2025-04-09 11:29:48 +0530

    Time: 16 ms (24.53%) | Memory: 32.5 MB (42.14%) - LeetSync

---
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        unordered_set<int> s;
+        for(int i:nums){
+            if(i<k){
+                return -1;
+            }
+            else if(i>k){
+                s.insert(i);
+            }
+        }
+        return s.size();
+        
+    }
+};

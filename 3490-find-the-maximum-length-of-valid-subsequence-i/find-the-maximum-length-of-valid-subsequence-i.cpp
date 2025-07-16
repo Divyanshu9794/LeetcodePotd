94990860a90e8b374c63989157e903adb78bf206
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2025-07-16 11:50:51 +0530

    Time: 9 ms (36.47%) | Memory: 96.4 MB (30.00%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> diff(2,0),same(2,0);
+        // int n = nums.size();
+        // vector<int> diff(2,0),same(2,0);
+        // for(int i=0;i<n;i++){
+        //     int current_remainder = nums[i]%2;
+        //     same[current_remainder] = 1+ same[current_remainder];
+        //     diff[current_remainder] = max(diff[current_remainder], 1+ diff[1^current_remainder]);
+        // }
+        // return max({same[0],same[1],diff[0],diff[1]});
+
+        vector<vector<int>> dp(2,vector<int>(2,0));
+        int n = nums.size(), ans =0;
         for(int i=0;i<n;i++){
             int current_remainder = nums[i]%2;
-            same[current_remainder] = 1+ same[current_remainder];
-            diff[current_remainder] = max(diff[current_remainder], 1+ diff[1^current_remainder]);
+            dp[current_remainder][0] = max(dp[current_remainder][0],1+dp[0][current_remainder]);
+            dp[current_remainder][1] = max(dp[current_remainder][1],1+dp[1][current_remainder]);
+            ans = max({ans,dp[current_remainder][0],dp[current_remainder][1]});
         }
-        return max({same[0],same[1],diff[0],diff[1]});
+
+        return ans;
+        
         
     }
 };

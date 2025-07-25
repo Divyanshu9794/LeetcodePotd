5fdc66640c7c19808c5e984e58574fdc3584b039
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2025-07-25 12:07:05 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (98.71%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,16 +1,23 @@
-//using tabulation
-
 class Solution {
 public:
-    
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n+2,vector<int>(2,0));
-        for(int i=n-1;i>=0;i--){
-            dp[i][1]= max(-prices[i]+dp[i+1][0],0+dp[i+1][1]);
-            dp[i][0]= max(prices[i]+dp[i+2][1],0+dp[i+1][0]);
-        }
-        return dp[0][1];
         
+        // Rolling variables
+        int aheadBuy = 0, aheadNotBuy = 0;     // dp[i+1][1], dp[i+1][0]
+        int ahead2Buy = 0, ahead2NotBuy = 0;   // dp[i+2][1], dp[i+2][0]
+
+        for(int i = n - 1; i >= 0; i--) {
+            int currBuy = max(-prices[i] + aheadNotBuy, aheadBuy);
+            int currNotBuy = max(prices[i] + ahead2Buy, aheadNotBuy);
+
+            // shift for next iteration
+            ahead2Buy = aheadBuy;
+            ahead2NotBuy = aheadNotBuy;
+            aheadBuy = currBuy;
+            aheadNotBuy = currNotBuy;
+        }
+
+        return aheadBuy; // we start with buy permission on day 0
     }
 };

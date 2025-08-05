354f392da6984bcdd0f291f701a27cfe4043a81c
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2025-08-05 12:35:14 +0530

    Time: 3 ms (75.71%) | Memory: 29.1 MB (12.75%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        vector<int> ans ;
+        int n = nums.size();
+        stack<int> st;
+        for(int i= 2*n-1 ;i>=0;i--){
+            while(!st.empty() && st.top()<= nums[i%n]){
+                st.pop();
+
+            }
+            if(i<n){
+                ans.push_back(st.empty()?-1:st.top());
+
+            }
+            st.push(nums[i%n]);
+
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};

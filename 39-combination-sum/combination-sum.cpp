e22a88c258ea0061ad743be4c3f1427a934e7e4e
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2025-08-05 10:45:48 +0530

    Time: 5 ms (35.76%) | Memory: 14.4 MB (42.70%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,48 @@
+
+class Solution {
+  public:
+    void generate(vector<int>& candidates,int ind, int target, vector<int>& ans, vector<vector<int>>& comb){
+        if(ind==candidates.size()){
+            if(target==0){
+                
+                comb.push_back(ans);
+            }
+            return;
+        }
+            
+        if(candidates[ind]<=target){
+            ans.push_back(candidates[ind]);
+            generate(candidates, ind, target-candidates[ind], ans, comb);
+            ans.pop_back();
+        }
+
+        generate(candidates, ind+1, target, ans, comb);
+            
+    }
+
+    vector<int> removeRepeated(vector<int>& a){
+        int n = a.size();
+        vector<int> unq;
+        unordered_map<int, int> m;
+    
+        for(int i=0;i<n;i++){
+            m[a[i]]++;
+        }
+        for(auto j: m){
+            unq.push_back(j.first);
+        }
+        return unq;
+    }
+    
+    vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
+        vector<vector<int>> comb;
+        vector<int> ans;
+        
+        sort(candidates.begin(), candidates.end());
+        candidates = removeRepeated(candidates);
+        sort(candidates.begin(), candidates.end());
+        
+        generate(candidates, 0, target, ans, comb);
+        return comb;
+    }
+};

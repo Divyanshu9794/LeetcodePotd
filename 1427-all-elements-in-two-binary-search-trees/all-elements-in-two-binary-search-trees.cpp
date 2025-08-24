f6da670dfe555c8679ef9fdd703d08403db3e7c2
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2025-08-24 22:33:05 +0530

    Time: 17 ms (10.10%) | Memory: 86.4 MB (66.80%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.cpp
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.cpp
@@ -11,20 +11,51 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root,vector<int>&ans){
-        if(root==NULL) return;
-        inorder(root->left,ans);
-        ans.push_back(root->val);
-        inorder(root->right,ans);
-    }
-    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        vector<int> ans;
-        if(root1==NULL && root2==NULL){
-            return {};
+    // void inorder(TreeNode* root,vector<int>&ans){
+    //     if(root==NULL) return;
+    //     inorder(root->left,ans);
+    //     ans.push_back(root->val);
+    //     inorder(root->right,ans);
+    // }
+    vector<int> merge(TreeNode *root1, TreeNode *root2) {
+        vector<int> res;
+        stack<TreeNode*> s1, s2;
+        
+        while (root1 || root2 || !s1.empty() || !s2.empty()) {
+            while (root1) {
+                s1.push(root1);
+                root1 = root1->left;
+            }
+            while (root2) {
+                s2.push(root2);
+                root2 = root2->left;
+            }
+            
+            if (s2.empty() || (!s1.empty() && s1.top()->val <= s2.top()->val)) {
+                root1 = s1.top();
+                s1.pop();
+                res.push_back(root1->val);
+                root1 = root1->right;
+            } 
+            else {
+                root2 = s2.top();
+                s2.pop();
+                res.push_back(root2->val);
+                root2 = root2->right;
+            }
         }
-        inorder(root1,ans);
-        inorder(root2,ans);
-        sort(ans.begin(),ans.end());
-        return ans;
+        return res;
+    }
+
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        // vector<int> ans;
+        // if(root1==NULL && root2==NULL){
+        //     return {};
+        // }
+        // inorder(root1,ans);
+        // inorder(root2,ans);
+        // sort(ans.begin(),ans.end());
+        // return ans;
+        return merge(root1,root2);
     }
 };

9cfb5f30443bbb41a1701ac26e05d5d293789305
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2026-04-13 12:55:11 +0530

    Time: 4 ms (49.82%) | Memory: 122 MB (41.68%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -10,49 +10,36 @@
  */
 class Solution {
 public:
-    ListNode* reverse(ListNode* head){
-        ListNode* curr=head;
-        ListNode* prev=NULL;
-        ListNode* next=NULL;
-        while(curr!=NULL){
-            next=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=next;
+    ListNode* reverse(ListNode* temp){
+        ListNode* t = temp;
+        ListNode* pre = NULL;
+        while(t!=NULL){
+            ListNode* front = t->next;
+            t->next = pre;
+            pre = t;
+            t =front;
         }
-        return prev;
-
-    }
-    ListNode* getmid(ListNode* head){
-        ListNode* slow=head;
-        ListNode* fast=head->next;
-        while(fast!=NULL && fast->next!=NULL){
-            fast=fast->next->next;
-            slow=slow->next;
-        }
-        return slow;
+        return pre;
     }
     bool isPalindrome(ListNode* head) {
-        if(head->next==NULL){
-            return true;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        ListNode* middle =getmid(head);
-        ListNode* temp = middle->next;
-
-        middle->next=reverse(temp);
-
-        ListNode* head1=head;
-        ListNode* head2=middle->next;
-        while(head2!=NULL){
-            if(head1->val != head2->val){
+        ListNode* newhead = reverse(slow->next);
+        ListNode* first = head;
+        ListNode* second = newhead;
+        while(second != NULL){
+            if(first->val!=second->val){
+                reverse(newhead);
                 return false;
             }
-            head1=head1->next;
-            head2=head2->next;
+            first = first->next;
+            second = second ->next;
         }
-         temp = middle->next;
-
-        middle->next=reverse(temp);
+        reverse(newhead);
         return true;
     }
 };

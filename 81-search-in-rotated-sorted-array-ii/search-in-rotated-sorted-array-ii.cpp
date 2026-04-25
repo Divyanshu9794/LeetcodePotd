20ef52f2a89a87969b6be62e7e3361fe3ecaa4d5
Divyanshu Singh <85441744+Divyanshu9794@users.noreply.github.com>
2026-04-25 20:04:28 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (27.70%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,36 +1,37 @@
+
 class Solution {
-public:
-    bool search(vector<int>& nums, int target) {
-        int low = 0, high = nums.size() - 1;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            if (nums[mid] == target) return true;
-
-            // When we can't determine the sorted part due to duplicates
-            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
-                low++;
-                high--;
+  public:
+    int search(vector<int>& arr, int key) {
+        // Code Here
+        int n = arr.size();
+        int low = 0, hi = n-1;
+        while(low<=hi){
+            int mid = (low + hi)/2;
+            if(arr[mid]==key){
+                return true;
             }
-            // Left half is sorted
-            else if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && target < nums[mid]) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
+            if(arr[low]== arr[mid] && arr[mid]==arr[hi]){
+                low++;
+                hi--;
+                continue;
+            }
+            if(arr[low]<=arr[mid]){
+                if(arr[low]<=key && key<= arr[mid]){
+                    hi = mid-1;
+                }
+                else{
+                    low = mid+1;
                 }
             }
-            // Right half is sorted
-            else {
-                if (nums[mid] < target && target <= nums[high]) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
+            else{
+                if(arr[mid]<= key && key<= arr[hi]){
+                    low = mid+1;
+                }
+                else{
+                    hi = mid-1;
                 }
             }
         }
-
         return false;
     }
 };
